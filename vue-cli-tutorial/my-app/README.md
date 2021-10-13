992dd7f3eb0ff19de83acddb41d467f99b9c6fad
nickguerra <nickg@maliciousgames.com>
2021-10-13 06:41:12 -0500

    Added new cli project to include stylus

--- vue-cli-tutorial/my-app/README.md
+++ vue-cli-tutorial/my-app/README.md
@@ -1,4 +1,4 @@
-# new-app
+# my-app
 
 ## Project setup
 ```
---
+++ vue-cli-tutorial/my-app/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+<div class="text-input">
+  <label>First Name</label>
+  <input v-model="firstName">
+</div>
+<div>
+  {{firstName}}
+</div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "App",
+  data() {
+return {
+  firstName: ""
+}
+  },
+  components: {
+  
+  },
+};
+</script>
+
+<style lang="stylus">
+#app
+  margin-top 60px
+
+  .text-input
+    label
+      display block
+</style>
