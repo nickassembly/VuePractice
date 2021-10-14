dcb3f7f334c51ed3aafd62c2aad6b6923fafb278
nickguerra <nickg@maliciousgames.com>
2021-10-14 06:25:54 -0500

    Worked with text field and validation

--- vue-cli-tutorial/my-app/src/App.vue
+++ vue-cli-tutorial/my-app/src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
-<div class="text-input">
+<div class="text-input" style="width: 200px">
+  <div>
   <label>First Name</label>
-  <input v-model="firstName">
+  <input :class="validationClass" v-model="firstName">
+  </div>
+  <div class="validation">
+{{inputCount}} / {{textLimit}}
+  </div>
 </div>
 <div>
   {{firstName}}
@@ -17,12 +22,30 @@ export default {
   name: "App",
   data() {
 return {
-  firstName: ""
+  firstName: "",
+  textLimit: 15
+};
+  },
+  watch: {
+firstName: function(newValue, oldValue) {
+  console.log(newValue, oldValue);
+  if (newValue.length > this.textLimit) this.firstName = oldValue;
+} 
+  },
+  computed: {
+inputCount() {
+  return this.firstName.length;
+},
+isEmpty() {
+  return this.firstName.length === 0;
+},
+validationClass() {
+  return this.isEmpty ? "invalid" : "valid";
 }
   },
   components: {
   
-  },
+  }
 };
 </script>
 
@@ -33,4 +56,18 @@ return {
   .text-input
     label
       display block
+      width 100%
+    
+    input 
+      display block
+    
+    input.valid
+      border 1px solid green
+
+    input.invalid
+      border 1px solid red
+
+    .validation
+      text-align right
+      font-size 12px
 </style>
