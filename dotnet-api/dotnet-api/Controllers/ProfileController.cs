a297809d5704c2f5837c4e52112346c79b0a7f44
nickguerra <nickg@maliciousgames.com>
2021-10-30 04:50:17 -0500

    Added crud operations to Profile controller

--- dotnet-api/dotnet-api/Controllers/ProfileController.cs
+++ dotnet-api/dotnet-api/Controllers/ProfileController.cs
@@ -2,11 +2,55 @@
 
 namespace dotnet_api.Controllers
 {
+    [Route("[controller]")]
+    [ApiController]
     public class ProfileController : Controller
     {
-        public string GetProfileName(string name)
+        [HttpGet]
+        public string GetProfiles()
         {
-            return $"hello world {name}";
+            return "getting all profiles";
         }
+
+        [HttpGet("{id}")]
+        public Profile GetProfile(int id)
+        {
+            return new Profile
+            {
+                Id = id,
+                FirstName = "Nick",
+                LastName = "foo",
+                Age = 42
+                
+            };
+        }
+
+        public class Profile
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+        }
+
+        [HttpPost]
+        public string CreateProfile(string profile)
+        {
+            return "";
+        }
+
+        [HttpPut]
+        public string UpdateProfile(string profile)
+        {
+            return "";
+        }
+
+        [HttpDelete]
+        public string DeleteProfile(int id)
+        {
+            return "";
+        }
+
+
     }
 }
--- dotnet-api/dotnet-api/Startup.cs
+++ dotnet-api/dotnet-api/Startup.cs
@@ -1,13 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace dotnet_api
 {
@@ -18,7 +12,6 @@ namespace dotnet_api
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
