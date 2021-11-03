ed3211ae85999c66ce068912137050344d03c350
nickguerra <nickg@maliciousgames.com>
2021-11-03 05:41:01 -0500

    Added button styles

--- vue-cli-tutorial/my-app/src/components/generic/SubmitButton.vue
+++ vue-cli-tutorial/my-app/src/components/generic/SubmitButton.vue
@@ -14,4 +14,17 @@ export default {
 button
     color white
     background #3794c1
+    padding 0.5rem 1rem 
+    border none
+    box-shadow 0 1px 3px 0 #000
+    transition ease-in 150ms
+    &:focus
+        outline none
+    &:hover
+        background #429dca
+        box-shadow 0 1px 3px 0 #000
+        cursor pointer
+    &:active
+        box-shadow none
+
 </style>
